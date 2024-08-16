e816c735a0a84079bc59c7b524951b71199265e2
MielAndMoon <dmestasa@gmail.com>
2024-08-16 12:44:16 -0500

    setup the style guide of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Be Vietnam Pro", "sans-serif"],
+      },
+      colors: {
+        "bright-red": "hsl(12, 88%, 59%)",
+        "dark-blue": "hsl(228, 39%, 23%)",
+        "dark-grayish-blue": "hsl(227, 12%, 61%)",
+        "very-dark-blue": "hsl(233, 12%, 13%)",
+        "very-pale-red": "hsl(13, 100%, 96%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+      backgroundImage: {
+        "close-menu": "url('../images/icon-close.svg')",
+        "open-menu": "url('../images/icon-hamburger.svg')",
+      },
+    },
   },
   plugins: [],
-}
+};
